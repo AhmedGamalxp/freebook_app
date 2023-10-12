3eb421aa5172b285ab542caf333c95211c7a0a3e
AhmedGamalxp <ahmedgamal.ag3010@gmail.com>
2023-10-12 22:57:33 +0300

    creating failures file and complet home repo impl

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:freebook_app/core/errors/failures.dart';
 import 'package:freebook_app/features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
-  Either<Failure, List<BookModel>> fetchFeaturedBooks();
-  Either<Failure, List<BookModel>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,49 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:freebook_app/core/errors/failures.dart';
+import 'package:freebook_app/core/utils/api_services.dart';
 import 'package:freebook_app/features/home/data/models/book_model/book_model.dart';
 import 'package:freebook_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoEmpl implements HomeRepo {
+  final ApiServices apiServices;
+  HomeRepoEmpl(this.apiServices);
   @override
-  Either<Failure, List<BookModel>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Either<Failure, List<BookModel>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
